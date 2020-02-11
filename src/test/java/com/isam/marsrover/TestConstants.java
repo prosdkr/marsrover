33cae6281e5eec8f4488829e44f105ce75ee794a
Syed Isam Hashmi <prosdkr@me.com>
2020-02-11 00:43:53 -0600

    removed unnecessary duplicate

--- src/test/java/com/isam/marsrover/TestConstants.java
+++ src/test/java/com/isam/marsrover/TestConstants.java
@@ -6,6 +6,4 @@ public class TestConstants {
     public static final String HOST = "localhost";
     public static final String HOST_URL = PROTOCOL + "://" + HOST + ":";
 
-
-    public static final String REST_V1 = "/rest/v1";
 }
